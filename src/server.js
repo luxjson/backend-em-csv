de96cf42ed47ed6fbbdb841b5148722d0fe0d417
LukzST <lucaseduarte6@gmail.com>
2026-05-19 13:52:11 -0300

    Atualiza rota raiz e importa dados CSV

--- src/server.js
+++ src/server.js
@@ -13,23 +13,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({
-    mensagem: 'API Estação Meteorológica',
-    descricao: 'API para consulta de leituras meteorológicas armazenadas no PostgreSQL.',
-    rotasDisponiveis: {
-      listarTodasAsLeituras: 'GET /api/leituras',
-      pesquisarLeiturasPorData: 'GET /api/leituras/data/2026-04-01',
-      pesquisarLeiturasImportadasPorCsv: 'GET /api/leituras/data/2026-04-02',
-    },
-    formatoDaData: 'YYYY-MM-DD',
-    exemplosDeUso: [
-      'http://localhost:3000/api/leituras',
-      'http://localhost:3000/api/leituras/data/2026-04-01',
-      'http://localhost:3000/api/leituras/data/2026-04-02',
-    ],
-  });
-});
+app.get('/', (req, res) => { 
+ return res.json({ 
+  mensagem: 'API Estação Meteorológica', 
+  descricao: 'API para consulta de leituras meteorológicas armazenadas no PostgreSQL.',  rotasDisponiveis: { 
+  listarTodasAsLeituras: 'GET /api/leituras', 
+  pesquisarLeiturasPorData: 'GET /api/leituras/data/2026-04-01', 
+  importarCsv: 'npm run import:csv' 
+  },
+  formatoDaData: 'YYYY-MM-DD', 
+  exemploDeUso: 'http://localhost:3000/api/leituras/data/2026-04-01', 
+  }); 
+}); 
+
 
 app.use('/api', leiturasRoutes);
 
